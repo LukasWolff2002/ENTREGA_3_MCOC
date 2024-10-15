1727ed180f52f4a026a34f9714f7b24201ef6011
LukasWolff2002 <lwolff@miuandes.cl>
2024-10-15 08:23:46 -0300

    avance;

--- CODIGO/LAPLACE/main.py
+++ CODIGO/LAPLACE/main.py
@@ -1,8 +1,8 @@
 from variables import caso_1, caso_2, caso_3, gamma_agua, gamma_sturada, caso_escalado
 from laplace_solver import laplace
 
-#laplace(caso_1, 'caso_1', 40)
-#laplace(caso_2, 'caso_2', 40)
-#laplace(caso_3, 'caso_3', 40)
+laplace(caso_1, 'caso_1', 40)
+laplace(caso_2, 'caso_2', 40)
+laplace(caso_3, 'caso_3', 40)
 
-laplace(caso_escalado, 'caso_escalado', 40)
+#laplace(caso_escalado, 'caso_escalado', 40)
